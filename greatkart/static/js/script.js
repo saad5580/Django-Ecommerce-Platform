3ba12e2666ca2c5a259a118999be15d57b5cece7
Saad <muhas6638@gmail.com>
2024-03-15 16:13:24 -0400

    product confirmation email, payment gateway, order confirmation page after payment completed

--- greatkart/static/js/script.js
+++ greatkart/static/js/script.js
@@ -58,6 +58,7 @@ $(document).ready(function() {
 }); 
 // jquery end
 
-setTimeout(function(){
-    $('#message').fadeOut('slow')
-}, 4000)
+setTimeout(function() {
+    $('#message').fadeOut('slow');
+}, 4000);
+
