4ade01cd98284e515c449663a1bd092404dd9694
Saad <muhas6638@gmail.com>
2024-09-04 15:49:03 -0400

    security changes

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -176,8 +176,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-
-
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
